af358d24734cc87e9d469e90f73817f7894f3d4a
Ross Halford <ross.halford@outlook.com>
2017-07-05 08:15:50 +0100

    added first-test.ps1

---
+++ First-Test.ps1
@@ -0,0 +1,24 @@
+#First Basic Pester Test
+
+Describe 'first test' {
+
+    Context 'arithmatic' {
+        it '1 plus 1 should be 2' {
+            1 + 1 | should be 2
+        }
+        it '3 minus 2 should be 1' {
+            3 - 2 | should be 1
+        }
+    }
+    Context 'AAA Approach' {
+
+        $stringToTest = 'team'
+
+        it 'an i should not be in team - like assertion' {
+            $stringToTest | should not belike '*i*'
+        }
+         it 'an i should not be in team - like assertion' {
+            $stringToTest | should not match 'i'
+         }
+    }
+}
